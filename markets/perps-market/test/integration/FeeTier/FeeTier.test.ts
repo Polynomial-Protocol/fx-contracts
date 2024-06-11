0ab7da8d375ac138c65b8d4379986128bbd8b9e3
arpit <arpitshukla62@gmail.com>
2024-06-11 20:32:43 +0530

    Add tests for computeOrderFees function

--- markets/perps-market/test/integration/FeeTier/FeeTier.test.ts
+++ markets/perps-market/test/integration/FeeTier/FeeTier.test.ts
@@ -1,5 +1,7 @@
-import { bootstrapMarkets } from '../bootstrap';
+import { bn, bootstrapMarkets } from '../bootstrap';
 import assertBn from '@synthetixio/core-utils/src/utils/assertions/assert-bignumber';
+import { wei } from '@synthetixio/wei';
+import { computeFees } from '../helpers';
 
 const feeTiers = [
   { id: 0, makerDiscount: 0, takerDiscount: 0 }, // 0% / 0%
@@ -9,9 +11,29 @@ const feeTiers = [
 ];
 
 describe('FeeTier', () => {
+  const orderFees = {
+    makerFee: wei(0.0003), // 3bps
+    takerFee: wei(0.0008), // 8bps
+  };
+  const ethPrice = bn(1000);
+  const ethMarketId = 25;
+
   const { systems, trader1 } = bootstrapMarkets({
     synthMarkets: [],
-    perpsMarkets: [],
+    perpsMarkets: [
+      {
+        requestedMarketId: ethMarketId,
+        name: 'Ether',
+        token: 'snxETH',
+        price: ethPrice,
+        // setting to 0 to avoid funding and p/d price change affecting pnl
+        fundingParams: { skewScale: bn(0), maxFundingVelocity: bn(0) },
+        orderFees: {
+          makerFee: orderFees.makerFee.toBN(),
+          takerFee: orderFees.takerFee.toBN(),
+        },
+      },
+    ],
     traderAccountIds: [1, 2, 3],
   });
 
@@ -28,19 +50,58 @@ describe('FeeTier', () => {
     await systems().PerpsMarket.connect(trader1()).updateFeeTier(2, 3, '0x');
   });
 
-  it('should return correct tier discounts', async () => {
-    for (const tier of feeTiers) {
-      const [makerDiscount, takerDiscount] = await systems()
-        .PerpsMarket.connect(trader1())
-        .getFeeTier(tier.id);
-      assertBn.equal(makerDiscount, tier.makerDiscount);
-      assertBn.equal(takerDiscount, tier.takerDiscount);
-    }
+  describe('getFeeTier', () => {
+    it('should return correct tier discounts', async () => {
+      for (const tier of feeTiers) {
+        const [makerDiscount, takerDiscount] = await systems()
+          .PerpsMarket.connect(trader1())
+          .getFeeTier(tier.id);
+        assertBn.equal(makerDiscount, tier.makerDiscount);
+        assertBn.equal(takerDiscount, tier.takerDiscount);
+      }
+    });
   });
 
-  it('should return correct fee tier for trading accounts', async () => {
-    assertBn.equal(await systems().PerpsMarket.connect(trader1()).getFeeTierId(1), 1);
-    assertBn.equal(await systems().PerpsMarket.connect(trader1()).getFeeTierId(2), 3);
-    assertBn.equal(await systems().PerpsMarket.connect(trader1()).getFeeTierId(3), 0);
+  describe('getFeeTierId', () => {
+    it('should return correct fee tier for trading accounts', async () => {
+      assertBn.equal(await systems().PerpsMarket.connect(trader1()).getFeeTierId(1), 1);
+      assertBn.equal(await systems().PerpsMarket.connect(trader1()).getFeeTierId(2), 3);
+      assertBn.equal(await systems().PerpsMarket.connect(trader1()).getFeeTierId(3), 0);
+    });
+  });
+
+  describe('computeOrderFees', () => {
+    it('should return base fees for tier zero', async () => {
+      const sizeDelta = bn(1);
+      const tentativeFeesPaid = computeFees(wei(0), wei(sizeDelta), wei(ethPrice), orderFees);
+      const [tentativeOrderFees] = await systems().PerpsMarket.computeOrderFeesWithPrice(
+        3, // tier 0 account
+        ethMarketId,
+        sizeDelta,
+        ethPrice
+      );
+      assertBn.equal(tentativeOrderFees, tentativeFeesPaid.perpsMarketFee);
+    });
+
+    it('should return discounted fees for non-zero tier', async () => {
+      const sizeDelta = bn(1);
+      const tentativeFeesPaidWithoutDiscount = computeFees(
+        wei(0),
+        wei(sizeDelta),
+        wei(ethPrice),
+        orderFees
+      );
+      const feeDiscount = 500; // 5% taker fee discount
+      const tentativePerpsMarketFeesPaid = tentativeFeesPaidWithoutDiscount.perpsMarketFee
+        .mul(10000 - feeDiscount)
+        .div(10000);
+      const [tentativeOrderFees] = await systems().PerpsMarket.computeOrderFeesWithPrice(
+        1, // tier 1 account
+        ethMarketId,
+        sizeDelta,
+        ethPrice
+      );
+      assertBn.equal(tentativeOrderFees, tentativePerpsMarketFeesPaid);
+    });
   });
 });
